ecff611750cba0ae446b96f6e544c909c1626b48
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2020-08-31 02:29:42 +0530

    Create Life, the Universe, and Everything.cpp

---
+++ Contest 1/Life, the Universe, and Everything.cpp
@@ -0,0 +1,40 @@
+//Keep calm and carry on..
+
+/*  Problem :Life, the Universe, and Everything
+    URL :https://www.codechef.com/LRNDSA01/problems/TEST
+
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+
+
+# define fast           ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+# define pb             push_back
+# define pi             3.1415926535897932384626
+# define mod            1000000007
+# define endl          '\n'
+# define DEBUG          false
+# define F              first
+# define S              second
+# define loop1(i,a,b)   for(ll i = a; i<b; i++)
+# define loop2(i,a,b)   for(ll i = b-1; i>=a; i--)
+# pragma GCC optimize "trapv"
+
+
+ int main(){
+     fast;
+     int x=0;
+     
+     while(x != 42){
+         cin>>x;
+         if(x != 42) cout<<x;
+         cout<<endl;
+     }
+
+  return 0;
+}
