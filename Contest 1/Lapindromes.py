b5c158933ee492f0131cb4e382bc6f19af338f35
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2020-10-27 05:17:21 +0530

    Create Lapindromes.py

---
+++ Contest 1/Lapindromes.py
@@ -0,0 +1,27 @@
+# Keep calm and carry on..
+
+'''  Problem :Lapindromes:LAPIN
+     URL :https://www.codechef.com/LRNDSA01/problems/LAPIN
+'''
+
+t = int(input())
+    
+while t >0 :
+    stng = input()
+    t -= 1
+    lh = len(stng)
+    if lh % 2 == 0 :
+        x = int(lh//2)
+        lfstr = stng[:x]
+        rgstr = stng[x:]
+    else :
+        x = int((lh-1) // 2)
+        lfstr = stng[:x]
+        x = int((lh+1) // 2)
+        rgstr = stng[x:]
+    lfstr = ''.join(sorted(lfstr))
+    rgstr = ''.join(sorted(rgstr))
+    if lfstr == rgstr :
+        print("YES")
+    else :
+        print("NO")
